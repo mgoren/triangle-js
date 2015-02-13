746f0fb80bea2eb40c50637187c6a25467e11a4f
Mike Goren <mgoren@mortalwombat.net>
2015-02-13 09:59:17 -0800

    fix syntax in js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 var triangle = function(side1, side2, side3) {
-  if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2)
+  if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
     return "not a triangle";
-  else if (side1 == side2 && side2 == side3)
+  }
+  else if (side1 == side2 && side2 == side3) {
     return "equilateral";
-  else if (side1 != side2 && side3 != side1 && side2 != side3)
+  }
+  else if (side1 != side2 && side3 != side1 && side2 != side3) {
     return "scalene";
-  else
+  }
+  else {
     return "isosceles";
-  end
+  }
 };
 
 // ------------------------------------------------------------------
@@ -15,18 +18,15 @@ var triangle = function(side1, side2, side3) {
 $(document).ready(function() {
   $("form#triangle-sides").submit(function(event) {
     
-    $("#order-result").text("");
-    $("#result").hide();
     $("#error").hide();
 
-    var phrase = $("input#phrase").val();
-    var words = orderify(phrase);
+    var side1 = $("input#side1").val();
+    var side2 = $("input#side2").val();
+    var side3 = $("input#side3").val();
 
-    $("#order-result").append("<ul>");
-    words.forEach(function(word) {
-      $("#order-result").append("<li>" + word + "</li>");
-    });
-    $("#order-result").append("</ul>");
+    var triangleType = triangle(side1, side2, side3));
+
+    $("#order p").text();
 
     if (phrase) {
       $("#result").show();
